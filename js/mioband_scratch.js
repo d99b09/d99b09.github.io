fe52bb4fca8918c65c0013c81b1fbef0fb13c5ef
Дима <d99b09@gmail.com>
2022-10-20 20:35:35 +0300

    added: main fun

--- js/mioband_scratch.js
+++ js/mioband_scratch.js
@@ -43,10 +43,58 @@ class MioBandMod{
     };
 }
 
+class MouseMod{
+    constructor(runtime) {
+        this.clear();
+    }
+
+    clear() {
+        this.socket = null;
+    }
+
+    getInfo() {
+        return{
+            "id": "MouseMod",
+            "name": "MouseControl",
+            "blocks": [
+                {
+                    "opcode": "substringy",
+                    "blockType": "reporter",
+                    "text": "letters [num1] through [num2] of [string]",
+                    "arguments": {
+                        "num1": {
+                            "type": "number",
+                            "defaultValue": "2"
+                        },
+                        "num2": {
+                            "type": "number",
+                            "defaultValue": "5"
+                        },
+                        "string": {
+                            "type": "string",
+                            "defaultValue": "hello world"
+                        }
+                    }
+                },
+            ],
+            "menus": {//later
+
+            }
+        }
+    }
+
+    substringy({num1, num2, string}) {
+        return string.substring(num1 - 1, num2);
+    };
+}
+
 (function() {
-    var extensionClass = MioBandMod
+    var extensionClass1 = MioBandMod
+    var extensionClass2 = MouseMod
     if (typeof window === "undefined" || !window.vm) {
-        Scratch.extensions.register(new extensionClass())
+        Scratch.extensions.register(new extensionClass1())
+        Scratch.extensions.register(new extensionClass2())
+
     }
     else {
         var extensionInstance = new extensionClass(window.vm.extensionManager.runtime)
