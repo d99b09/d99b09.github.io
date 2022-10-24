f98226175665309c10f35647cf26749fcccf5bdf
dima <d99b09@gmail.com>
2022-10-24 11:52:46 +0300

    second commit

--- js/mioband_scratch.js
+++ js/mioband_scratch.js
@@ -15,7 +15,7 @@ class MioBandMod{
             "id": "MioBandMod",
             "name": "MioBand",
             "blocks": [{
-                    "opcode": "testblock",
+                    "opcode": "miotestblock",
                     "blockType": "reporter",
                     "text": "Тестовый блок",
                 },
@@ -114,19 +114,19 @@ class MioBandMod{
         }
     }
 
-    // testblock(){
-    //     this.req.open('GET', this.ip + this.port + '/mioband/', false)
-    //     this.req.send()
-    //     this.req.onload = function () {
-    //         if (this.req.status !== 200){
-    //             alert('Error ${xhr.status}')
-    //         }
-    //         else{
-    //             alert(this.req.response)
-    //             return this.req.response
-    //         }
-    //     }
-    // }
+    miotestblock(){
+        this.req.open('GET', this.ip + this.port + '/mioband/', false)
+        this.req.send()
+        this.req.onload = function () {
+            if (this.req.status !== 200){
+                alert('Error ${xhr.status}')
+            }
+            else{
+                alert(this.req.response)
+                return this.req.response
+            }
+        }
+    }
 
     ifgestureh(){
         let istrue = True
